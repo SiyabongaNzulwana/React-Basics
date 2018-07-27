145c3ed22497266729c792ca6ba2473295984e5b
SiyabongaNzulwana <nzulwanas@yahoo.com>
2018-07-27 09:34:19 +0200

    did a child to parent communication via props and changed a react component to a stateless component

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -5,7 +5,8 @@ export class Home extends React.Component {
     super();
     this.state = {
       age: props.initialAge,
-      status: 0
+      status: 0,
+      homeLink2: "Changed Link"
     }
     setTimeout(() => {
       this.setState({
@@ -18,14 +19,20 @@ export class Home extends React.Component {
       age: this.state.age + 3
     })
   }
+  onChangeLink(){
+    this.props.changeLink(this.state.homeLink2);
+  }
   render() {
-    console.log(this.age);
     return (
       <div>
         <p>In a new Component</p>
         <p>Your name is {this.props.name}, and your age is {this.state.age}</p>
         <p>status: {this.state.status}</p>
         <button className='btn btn-primary' onClick={() => this.onMakeOlder()}>Make me older!</button>
+        <hr/>
+        <button className='btn btn-primary' onClick={this.props.greet}>Greet Me!</button>
+        <hr />
+        <button className='btn btn-primary' onClick={this.onChangeLink.bind(this)}>Change Link Header</button>
       </div>
     )
   }
--- src/app/index.js
+++ src/app/index.js
@@ -4,17 +4,38 @@ import { Header } from './components/Header';
 import { Home } from './components/Home';
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      homeLink :'Home'
+    }
+  }
+
+  onGreet(){
+    alert("Hello there!");
+  }
+
+  onChangeLinkHeader(newName){
+    this.setState({
+      homeLink: newName
+    });
+  }
+
   render() {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-xs-10 col-xs-offset-1'>
-            <Header />
+            <Header homeLink={this.state.homeLink}/>
           </div>
         </div>
         <div className='row'>
           <div className='col-xs-10 col-xs-offset-1'>
-            <Home name={'Siya'} initialAge={26}/>
+            <Home 
+            name={'Siya'} 
+            initialAge={26} 
+            greet={this.onGreet} 
+            changeLink={this.onChangeLinkHeader.bind(this)}/>
           </div>
         </div>
       </div>
